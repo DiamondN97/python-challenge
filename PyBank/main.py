2a08df3af73bc01f1f7fd945a70d3d61f85005d5
Nate <nathanieldiamond97@gmail.com>
2020-07-02 22:03:29 -0600

    total month output works

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,49 @@
+#import dependencies
 import os
 import csv
+values = []
+month_count = []
+
+
+#read csv
 
 datacsv = os.path.join('Resources', 'budget_data.csv')
 
-print('test')
+with open (datacsv, 'r') as datacsv_file:
+    csvReader = csv.reader (datacsv_file, delimiter = ',')
+    next (csvReader) 
+    for row in csvReader:
+        Date = row[0]
+        Profit = row[1]
+        month_count.append(row[0])        
+        #count number of months
+TotalMonth = len(month_count)
+
+print(TotalMonth)
+
+#find net total of P/L
+# profit_total = 0
+# month_total += 1
+# profit_change = 0
+
+# for row in csvReader:
+#     profit_total + int(Profit)
+#     month_total 
+
+
+#find the average of the changes of P/L
+
+#find greatest increase in profits
+
+#find greatest decrease in profits
+
+
+
+
+
+
+
+
+
+
+
