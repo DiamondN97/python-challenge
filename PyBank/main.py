0abc97c3237d19a48213ec28b0a3d90cc9db20c1
Nate <nathanieldiamond97@gmail.com>
2020-07-06 12:47:31 -0600

    added greatest decrease/increase of profit no date

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,7 @@ profit_delta = []
 average_change = 0
 
 
+
 #read csv
 datacsv = os.path.join('Resources', 'budget_data.csv')
 
@@ -22,9 +23,9 @@ with open ('Bootcamp/GitLab/python-challenge/PyBank/Resources/budget_data.csv', 
         Profit = row[1]
         month_count.append(Date) 
         profit_total.append(Profit)  
-        profit_delta.append(Profit)     
         #count number of months
 TotalMonth = len(month_count)
+
 
 print("Financial Analysis")
 print("----------------------------")
@@ -46,36 +47,37 @@ print(f'Total Profit: ${profit_sum}')
 
 # values=[int(i) for i in values] 
 # for i in range(0, len(values)-1):
-#     profit_delta[i] = profit_total[i] - profit_total[i+1]
-#     profit_delta.append(Profit[i] - Profit[i + 1])
+
 profit_change = [float(profit_total[i+1]) - float(profit_total[i]) for i in range(len(profit_total)-1)]
 
 average_change = (round(sum(profit_change) / len(profit_change), 2))
 print(f'Average Change: ${average_change}')
 
 
-#print(profit_change)
-
-#res = [test_list[i + 1] - test_list[i] for i in range(len(test_list)-1)]
-
-
-#print(profit_delta)
-
-# for i in range (0, len(profit_total)):
-#     profit_delta = (profit_total[i] + profit_total[i+1])
-#     #profit_delta.append(Profit[i]-Profit[i+1])
-# print (profit_delta)
-
-
-# for row in Profit:
-#     profit_change = int(sum(profit_delta))
-
-
-
-
 #find greatest increase in profits
+for i in range(0, len(profit_change)):
+    if profit_change > 
+    profit_max = max(profit_change)
+#print(profit_max)
+print(f'Greatest Increase in Profits: : (${profit_max})')
+
 
 #find greatest decrease in profits
+for i in range(0, len(profit_change)):
+    profit_min = min(profit_change)
+print(f'Greatest Decrease in Profits: : (${profit_min})')
+    
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+ 
+
+# for i in range(0,len(month_count)):
+#     profit_max_month = [(month_count, profit_max)]
+# print (profit_max_month)
+
 
 
 
