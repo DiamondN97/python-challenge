7acf9c9d51d0f16c383ac0ff57fc47a96cab4710
Nate <nathanieldiamond97@gmail.com>
2020-07-06 22:57:21 -0600

    output for candidates and vote totals

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,19 +26,19 @@ with open ('Bootcamp/GitLab/python-challenge/PyPoll/Resources/election_data.csv'
         candidate_list.append(row[2])
 VoteTotal = len(total_votes)
  
-#print("Election Results")
-#print("----------------------------")
+print("Election Results")
+print("----------------------------")
 
 #find total number of votes
 
-#print(f'Total Votes: {VoteTotal}')
+print(f'Total Votes: {VoteTotal}')
 print("----------------------------")
 
 
 
 CandidateName = list(set(candidate_list))
 CandidateName.sort()
-print(CandidateName)
+#print(CandidateName)
 
 # for i in candidate_list:
 #     if i == "Khan":
@@ -46,17 +46,47 @@ print(CandidateName)
 # print(khan_votes)
 
 #khan_votes = ((candidate_list.count((CandidateName[2]))/VoteTotal)*100
-khan_votes = float(candidate_list.count((CandidateName[1]))/VoteTotal)*100
-print (f'(khan votes: {khan_votes}')
+khan_votes_pct = round((float(candidate_list.count((CandidateName[1]))/VoteTotal)*100),4)
+khan_votes =  candidate_list.count((CandidateName[1]))
+print(f"Khan Votes: {khan_votes_pct}% ({khan_votes})")
 
-correy_votes = float(candidate_list.count((CandidateName[0]))/VoteTotal)*100
-print (f'(correy votes: {correy_votes}')
 
-li_votes = float(candidate_list.count((CandidateName[2]))/VoteTotal)*100
-print (f'(li votes {li_votes}')
+correy_votes_pct = round((float(candidate_list.count((CandidateName[0]))/VoteTotal)*100),4)
+correy_votes =  candidate_list.count((CandidateName[0]))
+print(f"Correy: {correy_votes_pct}% ({correy_votes})")
 
-tooley_votes = float(candidate_list.count((CandidateName[3]))/VoteTotal)*100
-print (f" (O'Tooley {tooley_votes}")
 
+li_votes_pct = round((float(candidate_list.count((CandidateName[2]))/VoteTotal)*100),4)
+li_votes = candidate_list.count((CandidateName[2]))
+print(f"Li: {li_votes_pct}% ({li_votes})")
+
+
+tooley_votes_pct = round((float(candidate_list.count((CandidateName[3]))/VoteTotal)*100),4)
+tooley_votes = candidate_list.count((CandidateName[3]))
+
+print(f"O'Tooley: {tooley_votes_pct}% ({tooley_votes})")
+print("----------------------------")
+
+# def popular_vote(candidate_list):
+#     counter = 0
+#     votes = candidate_list[0]
+#     for i in candidate_list:
+#         popularVotes = candidate_list.count(i)
+#         if(popularVotes > counter):
+#             counter = popularVotes
+#             CommonVotes = i 
+#     return CommonVotes
+# print(popular_vote(candidate_list))
+
+# popular_vote = {}
+# for i in candidate_list:
+#     popular_vote[i] = popular_vote.get(i, 0) + 1
+# WinnerList = popular_vote.keys()
+# # WinnerList.sort()
+# for CandidateName in WinnerList:
+   
+#     print (f'Winner: {CandidateName, popular_vote[CandidateName]}')
 #find candidate list
 #print(candidate_list) <--- total list of candidates
+
+  
